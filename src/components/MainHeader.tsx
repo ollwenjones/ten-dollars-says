40de507caf5b66faab379ab427e3a5e964d08f53
Sam Jones <sam@PAT.whq>
2018-11-09 13:36:15 -0500

    re-style header

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import logo from "../logo.svg";
 import { Login, SessionChangeHandler } from "./Login";
+import "./MainHeader.css";
 
 // tslint:disable-next-line:no-empty-interface
 export interface MainHeaderProps extends SessionChangeHandler {}
@@ -8,9 +9,10 @@ export interface MainHeaderProps extends SessionChangeHandler {}
 export default class MainHeader extends React.Component<MainHeaderProps, any> {
   public render() {
     return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Ten Dollars Says!</h1>
+      <header className="tds-header">
+        <img src={logo} className="tds-header__logo" alt="logo" />
+        <h1 className="tds-header__title">Ten Dollars Says...</h1>
+        <div className="tds-header__controls" />
         <Login onSessionChange={this.props.onSessionChange} />
       </header>
     );
