47758734c47788628bd9c850698d3a633bd15488
Sam Jones <sam@PAT.whq>
2018-11-14 16:39:44 -0500

    basic impl of leaderboard

--- src/components/LeaderBoard.tsx
+++ src/components/LeaderBoard.tsx
@@ -1,27 +1,61 @@
 import * as React from "react";
 import { BetsApi } from "src/rest-api/BetsApi";
+import "./LeaderBoard.css";
 
-export interface PartyReportState {
-  report: any[];
+export interface TdsParty {
+  party_name: string;
+  outcome: string;
+  created_on_date: Date;
+  entity_id: string;
+  entity_type_name: string;
 }
 
-export class LeaderBoard extends React.Component<{}, any> {
-  state = { report: [] };
+export interface LeaderBoardState {
+  winners: TdsParty[];
+}
+
+export class LeaderBoard extends React.Component<{}, LeaderBoardState> {
+  state: LeaderBoardState = { winners: [] };
 
   componentDidMount() {
-    BetsApi.fetchPartyReport().then(report => this.setState({ report }));
+    BetsApi.fetchPartyReport().then((report: any) =>
+      this.setState({ winners: report })
+    );
   }
+
+  // Be good to make a report that was key:value or CSV instead of an array
+  // for us to count client-side, but this is what we have for now:
+
+  getWinsByName = () => {
+    const countByName: { [key: string]: number } = {};
+    this.state.winners.map(party => {
+      if (countByName[party.party_name]) {
+        countByName[party.party_name] += 1;
+      } else {
+        countByName[party.party_name] = 1;
+      }
+    });
+    return countByName;
+  };
+
+  getLeaders = () => {
+    // charting library if there's time
+    const winners = this.getWinsByName();
+    return Object.keys(winners).map(winner => (
+      <tr key={winner}>
+        <td>{winner}</td>
+        <td>{winners[winner]}</td>
+      </tr>
+    ));
+  };
 
   public render() {
     return (
-      <section>
-        <h4>Leaderboard</h4>
-        <p>
-          Be good to make a report that was key:value or CSV instead of an array
-          for us to count client-side, but this is what we have for now:
-        </p>
-        <pre>{JSON.stringify(this.state.report, null, "  ")}</pre>; React C3js
-        Chart if there's time?
+      <section className="tds-leaderboard">
+        <h4 className="tds-leaderboard__title">Leaderboard</h4>
+        <table>
+          <tbody>{this.getLeaders()}</tbody>
+        </table>
       </section>
     );
   }
