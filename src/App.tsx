5da074c5432aa0e7df1f2bf4c654e2eab2ab0f5d
Sam Jones <sam@decisions.com>
2018-12-03 11:54:01 -0500

    update TODO list

--- src/App.tsx
+++ src/App.tsx
@@ -60,17 +60,11 @@ export default App;
 
 # Still need to
 
-1. fetch contacts
-    1. is the email thing okay? Seems like username without the email would be _preferable_
-1. Judge bet checkbox bug!?!
-1. Data refresh on create/judge API calls
-1. Loader(s)
+1. Prevent creating bets with empty name and empty parties.
+1. X to drop empty parties
+1. is the email thing okay? Seems like username without the email would be _preferable_
 1. Web sockets?
-1. tabbed table height CSS bug
-1. Make the wins highlights _pretty_
 1. Focus fields
-    1. Description on create bet
     1. Party name when adding a party
-    1. Add a field
 1. X close to Modal(s)
 */
